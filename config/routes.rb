a5e3edb40fcce6f928610037d02beb271bfeca1e
emmiiorji <emmanuelorji2013@gmail.com>
2022-11-16 07:50:14 +0100

    Generate controller and view for the splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'splash_screen#index'
+  # get 'splash_screen#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
