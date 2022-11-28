295b8b2b152901a700910fc2324f803ffa862eea
emmiiorji <emmanuelorji2013@gmail.com>
2022-11-28 03:44:53 +0100

    Modify model relationships to add foreign key of User in Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-    has_many :users
+  has_many :transacts
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :transacts, foreign_key: 'author_id'
+  has_many :categories
 end
