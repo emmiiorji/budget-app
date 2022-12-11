9bd4cd4b05ee5245bba66a9156d126134aa44760
emmiiorji <emmanuelorji2013@gmail.com>
2022-12-11 14:03:18 +0100

    Make transacts-categories relationsip many-to-many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_000129) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_125748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,13 +25,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_000129) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "categories_transacts", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transact_id", null: false
+    t.index ["category_id", "transact_id"], name: "index_categories_transacts_on_category_id_and_transact_id"
+    t.index ["transact_id", "category_id"], name: "index_categories_transacts_on_transact_id_and_category_id"
+  end
+
   create_table "transacts", force: :cascade do |t|
     t.bigint "author_id", null: false
-    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["author_id"], name: "index_transacts_on_author_id"
-    t.index ["category_id"], name: "index_transacts_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,6 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_000129) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "transacts", "categories"
   add_foreign_key "transacts", "users", column: "author_id"
 end
