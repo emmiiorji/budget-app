ad6a866c1b9b966e5f0281d6b6595fe892b9e609
emmiiorji <emmanuelorji2013@gmail.com>
2022-12-25 17:58:39 +0100

    Update transacts#create controller

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -15,12 +15,33 @@ class TransactsController < ApplicationController
   end
 
   def create
-    @transact = Transact.new(transact_params)
-    if @transact.save
-      flash[:success] = "Transaction created!"
-      redirect_to root_url
-    else
-      render 'new'
+
+    @category = Category.where(user_id: current_user.id).where(id: params[:category_id])[0]
+    
+    # If category does not exist, redirect to categories page
+    if @category.nil?
+      flash[:error] = "User did not create this category"
+      redirect_to categories_path
     end
+
+    @transact = @category.transacts.new(transact_params)
+
+    if @transact.save
+      @category.transacts << @transact
+      # CategoryTransacts.create(category_id: @category.id, transact_id: @transact.id)
+      flash[:success] = "Transaction created successfully!"
+      redirect_to category_path(@category)
+    else
+      flash[:error] = "Transaction could not be created"
+      render :new, locals: { category: @category }
+    end
+  end
+
+  private
+
+  def transact_params
+    transact = params.require(:transact).permit(:name, :amount)
+    transact[:author] = current_user
+    transact
   end
 end
