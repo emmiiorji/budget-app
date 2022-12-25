4a28e25ba214ecb7295484266c59d026293efd75
emmiiorji <emmanuelorji2013@gmail.com>
2022-12-25 17:57:01 +0100

    Update transacts#new controller

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -2,7 +2,16 @@ class TransactsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @transact = Transact.new
+    @category = Category.where(user_id: current_user.id).where(id: params[:category_id])[0]
+    
+    # If category does not exist, redirect to categories page
+    if @category.nil?
+      flash[:error] = "User did not create this category"
+      redirect_to categories_path
+    end
+
+    @transact = @category.transacts.new
+    @categories = Category.where(user_id: current_user.id)
   end
 
   def create
